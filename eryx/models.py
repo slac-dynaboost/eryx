fix: Bypass resolution masking for nonpositive res_limit in OnePhonon.

eryx/models.py
@@ -950,10 +950,12 @@ class OnePhonon:
                                                       return_hkl=True)
         log_array_shape(self.hkl_grid, "hkl_grid")
         if res_limit <= 0:
-            res_limit = -1
-        self.res_mask, res_map = get_resolution_mask(self.model.cell,
-                                                     self.hkl_grid,
-                                                     res_limit)
+            self.res_mask = np.ones(self.hkl_grid.shape[0], dtype=bool)
+            res_map = None
+        else:
+            self.res_mask, res_map = get_resolution_mask(self.model.cell,
+                                                         self.hkl_grid,
+                                                         res_limit)
         self.q_grid = 2 * np.pi * np.inner(self.model.A_inv.T, self.hkl_grid).T
         log_array_shape(self.q_grid, "q_grid")
 